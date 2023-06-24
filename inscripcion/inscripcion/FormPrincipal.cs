957f7a461b7fb5794f8ab20bed0d6c0daa62ccb0
ManuelGTM <manuelgabrielt@gmail.com>
2023-06-24 13:48:43 -0400

    Update of my 3 Stored Procedures

---
+++ inscripcion/inscripcion/FormPrincipal.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_inscripcion
+{
+    public partial class Sistema : Form
+    {
+        public Sistema()
+        {
+            InitializeComponent();
+        }
+
+        private void Sistema_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void periodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void consultaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tutorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porPeriodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porEmpleadoToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            statusStrip1.Items[1].Text = "Fecha/Hora: "+DateTime.Now.ToString();
+
+
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inscripcionToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void utilidadesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
