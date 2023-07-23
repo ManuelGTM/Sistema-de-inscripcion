e97fed58561641500764c9b38d1266cbce670a90
ManuelGTM <manuelgabrielt@gmail.com>
2023-07-22 22:31:04 -0400

    Arreglos

--- inscripcion/inscripcion/FMantEmpleado.Designer.cs
+++ inscripcion/inscripcion/FMantEmpleado.Designer.cs
@@ -463,6 +463,7 @@
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.MaximizeBox = false;
             this.Name = "FMantenimientoEmpleado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mantenimiento- Empleado";
             this.Load += new System.EventHandler(this.FMantenimientoEmpleado_Load);
             this.panel1.ResumeLayout(false);
